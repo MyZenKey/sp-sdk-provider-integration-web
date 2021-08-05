8b33ef71ce6952d9586f8a51fdc60affec420325
Garfield Heron <Garfield.Heron@myzenkey.com>
2021-08-05 14:28:01 -0400

    logout

--- Examples/PHP/auth/cb.php
+++ Examples/PHP/auth/cb.php
@@ -49,105 +49,137 @@ $error = filter_input_fix(INPUT_GET, 'error', FILTER_SANITIZE_STRING);
 $state = filter_input_fix(INPUT_GET, 'state', FILTER_SANITIZE_STRING);
 $code = filter_input_fix(INPUT_GET, 'code', FILTER_SANITIZE_STRING);
 $loginHintToken = filter_input_fix(INPUT_GET, 'login_hint_token', FILTER_SANITIZE_STRING);
-try {
-    // handle errors returned from ZenKey
-    if (isset($error)) {
-        $sessionService->clear();
-        throw new Exception($error);
-    }
+?>
 
-    // check if the user is already logged in
-    if ($sessionService->getCurrentUser() && !$authFlowHandler->authorizationInProgress()) {
-        header('Location: /');
 
-        return;
-    }
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>ZenKey-DemoApp-PHP</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="/stylesheets/zk-btn.css">
+  <link rel="stylesheet" href="/stylesheets/style.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top"><a class="navbar-brand" href="/">ZenKey-DemoApp-PHP</a>
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item">
+       <a class="nav-link" href="/logout.php">Sign Out</a>
+      ?>
+    </li>
+  </ul>
+</nav>
+<main class="container">
 
-    //  If we have no mccmnc, begin the carrier discovery process
-    if (!$mccmnc) {
-        header('Location: /auth.php');
+  <h1>Home</h1>
 
-        return;
-    }
 
-    if (!$state) {
-        // if an error happens, delete the auth information saved in the session
-        $sessionService->clear();
-        throw new Exception('missing state');
-    }
 
-    // build our OpenID client
-    $oidcProvider = $zenkeyOIDCService->discoverOIDCProvider($mccmnc);
-
-    // Request an auth code
-    // The carrier discovery endpoint has redirected back to our app with the mccmnc.
-    // Now we can start the authorize flow by requesting an auth code.
-    // Send the user to the ZenKey authorization endpoint. After authorization, this endpoint will redirect back to our app with an auth code.
-    if (!isset($code)) {
-        if ($authFlowHandler->authorizationInProgress()) {
-            // authorization is in progress: use a context and different ACR values
-            $authUrlOptions = [
-                'scope' => 'openid',
-                'context' => $authFlowHandler->getAuthorizationDetails()['context'],
-                'acr_values' => 'a3',
-            ];
-        } else {
-            $authUrlOptions = [
-                'scope' => $SCOPE,
-            ];
-        }
-
-        // send user to the ZenKey authorization endpoint to request an auth code
-        $authorizationUrl = urldecode($zenkeyOIDCService->getAuthCodeRedirectUrl($oidcProvider, $loginHintToken, $state, $mccmnc, $authUrlOptions));
-        header("Location: {$authorizationUrl}");
-
-        return;
-    }
-
-    // Request a token
-    // The authentication endpoint has redirected back to our app with an auth code. Now we can exchange the auth code for a token.
-    // Once we have a token, we can make a userinfo request to get user profile information.
-    if (isset($code)) {
-        $tokenVerificationValues = [];
-        if ($authFlowHandler->authorizationInProgress()) {
-            // validate the token with all the auth request details
-            // TODO: currently not all carriers return a context, or return it in different formats (string vs base64 encoded)
-            // re-enable context validation once carriers are consistent
-            // $tokenVerificationValues['context'] = $authFlowHandler->getAuthorizationDetails()['context'];
-            $tokenVerificationValues['acrValues'] = 'a3';
-            $tokenVerificationValues['sub'] = $sessionService->getCurrentUser()['sub'];
-        }
-
-        $tokens = $zenkeyOIDCService->requestToken($oidcProvider, $code, $state, $tokenVerificationValues);
-
-        // if an authorization is in progress, return the authflowhandler success router
-        if ($authFlowHandler->authorizationInProgress()) {
-            return $authFlowHandler->successRouter();
-        }
-
-        // make a userinfo request to get user information
-        $userInfo = $zenkeyOIDCService->getUserinfo($oidcProvider, $tokens);
-
-        // this is where a real app might look up the user in the database using the "sub" value
-        // we could also create a new user or show a registration form
-        // the $userInfo object contains values like sub, name, and email (depending on which scopes were requested)
-        // these values can be saved for the user or used to auto-populate a registration form
-
-        // save the user info in the session
-        $sessionService->setCurrentUser($userInfo->toArray());
-
-        // now that we are logged in, return to the homepage
-        header('Location: /');
-
-        return;
-    }
-
-    // If we have no mccmnc, begin the carrier discovery process
-    header('Location: /auth.php');
-} catch (Exception $e) {
-    $sessionService->clearState();
-    error_log($e);
-    // for the sake of this example we log the error message
-    // a real app might show a user-friendly error message instead
-    echo filter_var($e->getMessage(), FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-}
+</main>
+</body>
+</html>
+//try {
+//    // handle errors returned from ZenKey
+//    if (isset($error)) {
+//        $sessionService->clear();
+//        throw new Exception($error);
+//    }
+//
+//    // check if the user is already logged in
+//    if ($sessionService->getCurrentUser() && !$authFlowHandler->authorizationInProgress()) {
+//        header('Location: /');
+//
+//        return;
+//    }
+//
+//    //  If we have no mccmnc, begin the carrier discovery process
+//    if (!$mccmnc) {
+//        header('Location: /auth.php');
+//
+//        return;
+//    }
+//
+//    if (!$state) {
+//        // if an error happens, delete the auth information saved in the session
+//        $sessionService->clear();
+//        throw new Exception('missing state');
+//    }
+//
+//    // build our OpenID client
+//    $oidcProvider = $zenkeyOIDCService->discoverOIDCProvider($mccmnc);
+//
+//    // Request an auth code
+//    // The carrier discovery endpoint has redirected back to our app with the mccmnc.
+//    // Now we can start the authorize flow by requesting an auth code.
+//    // Send the user to the ZenKey authorization endpoint. After authorization, this endpoint will redirect back to our app with an auth code.
+//    if (!isset($code)) {
+//        if ($authFlowHandler->authorizationInProgress()) {
+//            // authorization is in progress: use a context and different ACR values
+//            $authUrlOptions = [
+//                'scope' => 'openid',
+//                'context' => $authFlowHandler->getAuthorizationDetails()['context'],
+//                'acr_values' => 'a3',
+//            ];
+//        } else {
+//            $authUrlOptions = [
+//                'scope' => $SCOPE,
+//            ];
+//        }
+//
+//        // send user to the ZenKey authorization endpoint to request an auth code
+//        $authorizationUrl = urldecode($zenkeyOIDCService->getAuthCodeRedirectUrl($oidcProvider, $loginHintToken, $state, $mccmnc, $authUrlOptions));
+//        header("Location: {$authorizationUrl}");
+//
+//        return;
+//    }
+//
+//    // Request a token
+//    // The authentication endpoint has redirected back to our app with an auth code. Now we can exchange the auth code for a token.
+//    // Once we have a token, we can make a userinfo request to get user profile information.
+//    if (isset($code)) {
+//        $tokenVerificationValues = [];
+//        if ($authFlowHandler->authorizationInProgress()) {
+//            // validate the token with all the auth request details
+//            // TODO: currently not all carriers return a context, or return it in different formats (string vs base64 encoded)
+//            // re-enable context validation once carriers are consistent
+//            // $tokenVerificationValues['context'] = $authFlowHandler->getAuthorizationDetails()['context'];
+//            $tokenVerificationValues['acrValues'] = 'a3';
+//            $tokenVerificationValues['sub'] = $sessionService->getCurrentUser()['sub'];
+//        }
+//
+//        $tokens = $zenkeyOIDCService->requestToken($oidcProvider, $code, $state, $tokenVerificationValues);
+//
+//        // if an authorization is in progress, return the authflowhandler success router
+//        if ($authFlowHandler->authorizationInProgress()) {
+//            return $authFlowHandler->successRouter();
+//        }
+//
+//        // make a userinfo request to get user information
+//        $userInfo = $zenkeyOIDCService->getUserinfo($oidcProvider, $tokens);
+//
+//        // this is where a real app might look up the user in the database using the "sub" value
+//        // we could also create a new user or show a registration form
+//        // the $userInfo object contains values like sub, name, and email (depending on which scopes were requested)
+//        // these values can be saved for the user or used to auto-populate a registration form
+//
+//        // save the user info in the session
+//        $sessionService->setCurrentUser($userInfo->toArray());
+//
+//        // now that we are logged in, return to the homepage
+//        header('Location: /');
+//
+//        return;
+//    }
+//
+//    // If we have no mccmnc, begin the carrier discovery process
+//    header('Location: /auth.php');
+//} catch (Exception $e) {
+//    $sessionService->clearState();
+//    error_log($e);
+//    // for the sake of this example we log the error message
+//    // a real app might show a user-friendly error message instead
+//    echo filter_var($e->getMessage(), FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+//}
